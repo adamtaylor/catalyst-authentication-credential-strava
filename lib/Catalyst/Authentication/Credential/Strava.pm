f7fe57a625693fbca91cbdeab941a998b4432f4c
Adam Taylor <adamjctaylor@gmail.com>
2015-08-20 07:57:22 +0100

    Add documentation

--- lib/Catalyst/Authentication/Credential/Strava.pm
+++ lib/Catalyst/Authentication/Credential/Strava.pm
@@ -22,6 +22,20 @@ package Catalyst::Authentication::Credential::Strava;
         },
     );
 
+    package MyApp::Controller::Login;
+
+    sub login : Local {
+        my ( $self, $c ) = @_;
+
+        # Redirects to Strava for the user to login and authorise the application.
+        # Uses the returned access token to lookup a user in the store.
+        my $user = $c->authenticate();
+
+        $c->detach unless $user;
+
+        # Do any post login actions you like
+    }
+
 =head1 DESCRIPTION
 
 Authenticate your Catalyst application's users using L<Strava's OAuth API|https://www.strava.com/developers>.
@@ -30,11 +44,11 @@ Authenticate your Catalyst application's users using L<Strava's OAuth API|https:
 
 =head2 C<client_id>
 
-This is provided by Strava's API when you set up your application.
+This is provided by Strava's API when you register your application with their API.
 
 =head2 C<client_secret>
 
-This is provided by Strava's API when you set up your application.
+This is provided by Strava's API when you register your application with their API
 
 =head1 METHODS
 
@@ -42,7 +56,28 @@ This is provided by Strava's API when you set up your application.
 
     my $user = $c->authenticate();
 
-Attempts to authenticate a user using Strava's OAuth API.
+Attempts to authenticate a user using Strava's OAuth API. Because this is an OAuth
+authentication provider this will cause a redirect to the Strava website, where
+the user can login or register, which will then redirect back to your Catalyst
+application with an access token.
+
+This token (C<strava_access_token>) is used to look up a user and return one, if
+found, or undef. An exception is thrown if the access token retreival fails.
+
+If you just want to retrieve the token, you should look at
+L<Catalyst::Authentication::Store::Null> otherwise if you use something like
+L<Catalyst::Authentication::Store::DBIx::CLass> your user table shold have a column
+C<strava_access_token> for user lookup.
+
+=head1 CUSTOMISATION
+
+If you want to customise the behavior of this module, please take a look at
+L<Catalyst::Authentication::Realm::Adaptor>.
+
+=head1 ACKNOWLEDGEMENTS
+
+This is basically L<Catalyst::Authentication::Credential::Facebook::OAuth2> made
+to work with Strava with the help of L<LWP::Authen::OAuth2>.
 
 =cut
 
